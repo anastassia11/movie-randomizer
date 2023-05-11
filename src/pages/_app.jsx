ec2ff423fd7496a7b51982c746b912a6f2003ba3
anastassia11 <114300806+anastassia11@users.noreply.github.com>
2023-05-12 00:39:44 +0700

    feat: added movies data request

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,18 @@
 import Footer from '@/components/footer/Footer'
 import '@/styles/globals.css'
+import { BASE_URL } from '@/utils/constants'
+import axios from 'axios'
 
 export default function App({ Component, pageProps }) {
+  console.log(pageProps)
   return <div className='flex flex-col h-screen items-center justify-between p-7'>
     <Component {...pageProps} />
     <Footer />
   </div>
+}
+
+App.getInitialProps = async ({ Component }) => {
+  const pageProps = Component.getInitialProps
+  const { data } = await axios.get(`${BASE_URL}/api/movies`)
+  return { pageProps: { ...pageProps, data } }
 }
---
+++ src/pages/api/movies.js
@@ -0,0 +1,11 @@
+import { request } from '@/utils/common'
+
+export default async function handler(req, res) {
+  try {
+    const { data } = await request({ url: 'title/get-most-popular-movies' })
+    res.status(200).json({ data })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json('Error during fetching movies')
+  }
+}
